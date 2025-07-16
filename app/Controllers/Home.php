be5c8404a5123e7f81f6cde8b80761c356c0ecba
Ever-Alvarez <ever.garzaalvarez25@gmail.com>
2025-07-16 17:31:58 -0600

    Add: metodo de hash

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,7 +5,9 @@ namespace App\Controllers;
 class Home extends BaseController
 {
     public function index(): string
-    {
+    {   
+        // $pass = password_hash('password', PASSWORD_DEFAULT);
+        // echo $pass;
         return view('welcome_message');
     }
 }
