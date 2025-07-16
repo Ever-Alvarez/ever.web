51b98a87da6df30690a14dee7fefd079a6658247
Ever-Alvarez <ever.garzaalvarez25@gmail.com>
2025-07-16 11:50:20 -0600

    Add: front-end login

---
+++ client/src/Login/Login.jsx
@@ -0,0 +1,18 @@
+import '../assets/css/styles.css'
+
+function Login(){
+    return (
+    
+            <div className="bodyLogin">
+                <div className="center">
+                    CENTER
+                </div>
+                <div className="col-md-12">
+                    HOLA MUNDO
+                </div>
+            </div>
+    
+    )
+}
+
+export default Login
