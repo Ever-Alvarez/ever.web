20298068a5552515823a9a33d668b47c67359726
Ever-Alvarez <ever.garzaalvarez25@gmail.com>
2025-07-16 12:42:22 -0600

    Add: login react corregido

--- client/src/Login/Login.jsx
+++ client/src/Login/Login.jsx
@@ -4,11 +4,24 @@ function Login(){
     return (
     
             <div className="bodyLogin">
-                <div className="center">
-                    CENTER
+                <div className="centerLogin">
+                    {/* CENTER */}
+                <div className="form">
+                    <form className="form-horizontal" action="" method="post">
+                        <div className="form-group">
+                            <div className="col-md-9">
+                                <label htmlFor="user">Usuario:</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-md-9">
+                                <label htmlFor="pass">Contraseña:</label>
+                                <input type="password" className="form-control" />
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div className="col-md-12">
-                    HOLA MUNDO
                 </div>
             </div>
     
